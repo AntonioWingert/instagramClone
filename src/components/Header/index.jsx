1565126db9b4197eec7cbb9b89e83c7496e7de13
Antonio Bruno Wingert <antoniobwingert@gmail.com>
2022-10-24 08:36:01 -0300

    header complet

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
+import "./style.css";
 import { AiOutlineSearch, AiFillHome } from 'react-icons/ai';
 import { RiMessengerLine } from 'react-icons/ri';
 import { BsPlusSquare } from 'react-icons/bs';
 import { MdOutlineExplore } from 'react-icons/md';
 import { FiHeart } from 'react-icons/fi';
-import "./style.css";
 import { IconContext } from 'react-icons';
 
 export default class Header extends Component {
@@ -21,11 +21,21 @@ export default class Header extends Component {
          </div>
          <div className="menu-icons">
           <IconContext.Provider value={{ size: '26px' }}>
-            <AiFillHome />
-            <RiMessengerLine />
-            <BsPlusSquare />
-            <MdOutlineExplore />
-            <FiHeart />
+            <div>
+              <AiFillHome />
+            </div>
+            <div>
+              <RiMessengerLine />
+            </div>
+            <div>
+              <BsPlusSquare />
+            </div>
+            <div>
+              <MdOutlineExplore />
+            </div>
+            <div>
+              <FiHeart />
+            </div>
           </IconContext.Provider>
           <img src="https://scontent.figu2-1.fna.fbcdn.net/v/t39.30808-6/281614199_736829414015709_960379493275792156_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=EdCQMm4igDIAX-wYTzQ&_nc_ht=scontent.figu2-1.fna&oh=00_AT8kx4sompAZif6VMF8OGp8oEv6CuaqlQQug6BQTtNRhXA&oe=635AA4D9" alt="imagem-usuario" className="img-user" />
          </div>
